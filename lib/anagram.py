e9203a802a5e006a99a0bda1aad39e0229e08575
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-05-12 20:59:44 +0300

    assignment submission

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+#Pseudocode 
+#create a class called Anagram and should take word on the intialization
+#we will loop through the word to se if the letters matches the word given
+#Return a list of all matches, otherWise retrn an empty array
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted( letter for letter in word)
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted ([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list
